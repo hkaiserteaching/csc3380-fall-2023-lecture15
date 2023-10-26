6f8eac99bd3998956cd0bb924e272a33944cdaa1
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-10-26 12:38:51 -0500

    Adding FLTK

--- code/display_model.cpp
+++ code/display_model.cpp
@@ -1,20 +1,24 @@
-#include <ft.h>
+// Getting access to the graphics system
+#include "Graph.h"            // graphical shapes
+#include "Simple_window.h"    // stuff to deal with your system's windows
 
 int main()
 {
-    using namespace Graph_lib;    // use our graphics interface library
+    Graph_lib::Point tl(100, 200);    // a point (obviously)
 
-    Point tl(100, 200);    // a point (obviously)
-
+    // screen coordinate (100, 200) top left of window
+    // window size(600*400)
+    // title: Canvas
     Simple_window win(tl, 600, 400, "Canvas");    // make a simple window
 
-    Polygon poly;    // make a shape (a polygon, obviously)
+    Graph_lib::Polygon poly;    // make a shape (a polygon, obviously)
 
-    poly.add(Point(300, 200));    // add three points
-    poly.add(Point(350, 100));
-    poly.add(Point(400, 200));
+    poly.add(Graph_lib::Point(300, 200));    // add three points
+    poly.add(Graph_lib::Point(350, 100));
+    poly.add(Graph_lib::Point(400, 200));
 
-    poly.set_color(Color::red);    // make the polygon red (obviously)
+    poly.set_color(
+        Graph_lib::Color::red);    // make the polygon red (obviously)
 
     win.attach(poly);    // connect poly to the window
 
